4827a4cc6e6d342bf3e09fdcfebd0b9b41b5d492
Sherrisa Classon <sherrisaclasson@outlook.com>
2021-12-27 13:42:45 -0800

    First Commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div class="jumbotron">
+            This is the header.
+        </div>
+    )
+}
+
+export default Header
