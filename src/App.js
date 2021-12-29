6b22953af23fd20d269419b6bc92ec7ad12b44e1
Sherrisa Classon <sherrisaclasson@outlook.com>
2021-12-28 17:41:32 -0800

    Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Header from './components/Header';
-import Footer from './components/Footer';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './pages/Home';
 import Investment from './pages/Investment.js';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact.js';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div>
-      Footer goes here.
-    </div>
+    <footer >
+      <div className="container">
+        <div className="row">
+          <div className="col py-5 ml-3">
+            <h4>Leah Glasscock Photography</h4>
+            <p>From maternity to graduation</p>
+          </div>
+          <div className="col text-center py-5">
+            <h4>Links</h4>   
+            <ul className="list-unstyled">
+              <li><Link to='/investment'>Investment</Link></li>
+              <li><Link to='/portfolio'>Portfolio</Link></li>
+              <li><Link to='/contact'>Contact</Link></li>
+            </ul>
+          </div>
+          <div className="col text-center py-5">
+            <div>
+              <h4>Get in Touch</h4>
+              <div>
+                <a href="tel:+12065551234"><i className="fa fa-phone"></i> 1-253-555-1234</a><br />
+                <a href="mailto:leah@leahglasscock.com"><i className="fa fa-envelope-o"></i> leah@leahglasscock.com</a>
+              </div>  
+            </div>             
+          </div>   
+        </div>          
+      </div>
+    </footer>
   )
 }
 
